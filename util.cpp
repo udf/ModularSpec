c5359a823f925a8120892438e286e2bc2a519926
udf <tabhooked@gmail.com>
2019-10-26 23:22:52 +0200

    Make map inline

--- util.cpp
+++
@@ -1,5 +0,0 @@
-// Maps a value from one range to another
-// taken from Processing: https://stackoverflow.com/a/17135426
-float map(float n, float min1, float max1, float min2, float max2) {
-    return min2 + (max2 - min2) * ((n - min1) / (max1 - min1));
-}
--- util.h
+++ util.h
@@ -1,1 +1,7 @@
-float map(float n, float min1, float max1, float min2, float max2);
+#pragma once
+
+// Maps a value from one range to another
+// taken from Processing: https://stackoverflow.com/a/17135426
+inline float map(float n, float min1, float max1, float min2, float max2) {
+    return min2 + (max2 - min2) * ((n - min1) / (max1 - min1));
+}
